016ae71f45f90bd86133e842a9916a00ff24546e
Jolfr <thomas.jack.carroll@gmail.com>
2020-12-06 14:06:00 -0500

    added stub section component

---
+++ src/components/Section.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+class Section extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <></>
+    )
+  }
+}
