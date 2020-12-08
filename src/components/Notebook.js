ba4e4d1c49585d00b853062afbbe214421e6aa41
Jolfr <thomas.jack.carroll@gmail.com>
2020-12-08 09:18:27 -0500

    added collapsible to notebook object

--- src/components/Notebook.js
+++ src/components/Notebook.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import { graphql } from 'gatsby'
+import Collapsible from './Collapsible'
 
 /**
  * Receives mapped object from Section.js
@@ -19,16 +20,18 @@ class Notebook extends React.Component {
     const { markdownRemark } = this.props.data // data.markdownRemark holds your post data
     const { frontmatter, html } = markdownRemark
     return (
-      <div className="blog-post-container">
-        <div className="blog-post">
-          <h1>{frontmatter.title}</h1>
-          <h2>{frontmatter.date}</h2>
-          <div
-            className="blog-post-content"
-            dangerouslySetInnerHTML={{ __html: html }}
-          />
+      <Collapsible title={frontmatter.title}>
+        <div className="blog-post-container">
+          <div className="blog-post">
+            <h1>{frontmatter.title}</h1>
+            <h2>{frontmatter.date}</h2>
+            <div
+              className="blog-post-content"
+              dangerouslySetInnerHTML={{ __html: html }}
+            />
+          </div>
         </div>
-      </div>
+      </Collapsible>
     )
   }
 }
