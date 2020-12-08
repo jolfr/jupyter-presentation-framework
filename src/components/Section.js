21b6e79b5165432f9bd86f6768ddc5bdfc2e7f7f
Jolfr <thomas.jack.carroll@gmail.com>
2020-12-08 08:33:38 -0500

    added architecture note

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,16 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
+/**
+ * Implements structure for section of project (ex. data-cleaning)
+ * Should render popup with all attached notebooks
+ * This is the point that each notebook file is read in from
+ * graphql and mapped to each notebook section
+ * Each notebook should be hidden under a collapsible section
+ * rendered with the notebook name in readable format. Can probably use
+ * header injection here.
+ *
+ */
 class Section extends React.Component {
   constructor(props) {
     super(props);
