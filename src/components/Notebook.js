5ecc4d19d73d3ff86ea8f8924467c341f64d9f8b
Jolfr <thomas.jack.carroll@gmail.com>
2020-12-08 08:41:35 -0500

    added stub notebook file for rendering actual notebooks

---
+++ src/components/Notebook.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+/**
+ * Receives mapped object from Section.js
+ * and sets up proper structure with collapsible.
+ * Reads in all data from graphql and uses notebook
+ * template for rendering actual notebook.
+ */
+
+class Notebook extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+}
+
+Notebook.propTypes = {
+  
+}
