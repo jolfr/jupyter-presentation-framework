9b64f8ab6c72322eff8370ece8361249cfeb0ca5
Jolfr <thomas.jack.carroll@gmail.com>
2020-12-08 09:21:00 -0500

    changed comments to reflect new structure

--- src/components/Notebook.js
+++ src/components/Notebook.js
@@ -6,8 +6,8 @@ import Collapsible from './Collapsible'
 /**
  * Receives mapped object from Section.js
  * and sets up proper structure with collapsible.
- * Reads in all data from graphql and uses notebook
- * template for rendering actual notebook.
+ * Reads in all data from graphql and renders
+ * notebook
  */
 
 class Notebook extends React.Component {
