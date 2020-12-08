62be14a23fc7bb6be20e2aa1d442d3e4189dfc67
Jolfr <thomas.jack.carroll@gmail.com>
2020-12-08 09:14:27 -0500

    refactored notebook template to notebook

--- src/components/Notebook.js
+++ src/components/Notebook.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import { graphql } from 'gatsby'
 
 /**
  * Receives mapped object from Section.js
@@ -15,12 +16,45 @@ class Notebook extends React.Component {
   }
 
   render() {
+    const { markdownRemark } = this.props.data // data.markdownRemark holds your post data
+    const { frontmatter, html } = markdownRemark
     return (
-      <></>
+      <div className="blog-post-container">
+        <div className="blog-post">
+          <h1>{frontmatter.title}</h1>
+          <h2>{frontmatter.date}</h2>
+          <div
+            className="blog-post-content"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
+      </div>
     )
   }
 }
 
 Notebook.propTypes = {
-
+  data: {
+    markdownRemark: {
+      html: PropTypes.any.isRequired,
+      frontmatter: {
+        date: PropTypes.any.isRequired,
+        slug: PropTypes.any.isRequired,
+        title: PropTypes.any.isRequired,
+      }
+    }
+  }
 }
+
+export const pageQuery = graphql`
+  query($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        slug
+        title
+      }
+    }
+  }
+`
