02ed82e88a12bc8b310916d75ee18013be6f711b
Jolfr <thomas.jack.carroll@gmail.com>
2020-12-14 13:35:48 -0500

    added table parsing

--- plugins/gatsby-remark-table-formatter/index.js
+++ plugins/gatsby-remark-table-formatter/index.js
@@ -5,32 +5,56 @@ module.exports = ({ markdownAST }, options) => {
   const {useDefaultStyles = true, className = ""} = options;
   visit(markdownAST, "code", (node) => {
     let code = toString(node)
+    let searchGrid = code.replace(/(\r\n|\n|\r)/gm, '') // remove newlines
     const syntax = /\+-.*-\+/ig
-    const matches = code.match(syntax);
-
+    const matches = searchGrid.match(syntax);
 
     if (matches !== null) {
-      let style = null
-      if (useDefaultStyles) {
-        style = `
-          display:inline-block;
-          padding:5px;
-          background-color:yellow;
-          color:black;
-          border-radius: 5px;
-        `
+      var parseRow = (row, brackets) => {
+        var newRow = ''
+        var openBracket = brackets[0];
+        var closeBracket = brackets[1];
+        const syntax = /\|\s*\+*(\w)*\.*/ig
+        const cols = row.match(syntax);
+        cols.map(col => {
+          col = col.replace(/\|/,"").replace(/\s*/,"")
+          if(col !== ''){
+            col = openBracket + col + closeBracket
+            newRow = newRow + col
+          }
+        })
+        return newRow
       }
-      const putTextInSpan = text =>
-        `<span
-          ${useDefaultStyles && style ? ` style='${style}'` : ""}
-          ${className !== "" ? `class='${className}'` : ""}
-        >${text}</span>`
 
-      matches.map(match => {
-        code = code.replace(match, putTextInSpan(match))
-      })
+      var char = '\n';
+      var i = 0;
+      var j = 0;
+      var lineCount = 0; //NOTE: this is count of +---+ lines, not all lines
+      var newHeader = ''
+      var newBody = ''
+      var newFooter = ''
 
-      code = '<pre><p>' + code + '</p></pre>'
+      while ((j = (code + char).indexOf(char, i))!==
+      -1) {
+        var line = code.substring(i, j);
+        if (line.match(/\+-.*-\+/ig)) {
+          lineCount++;
+        } else if (lineCount===1) {
+          line = parseRow(line,['\t<th><div>','</div></th>\n'])
+          line = '<tr>\n' + line + '</tr>\n'
+          newHeader = newHeader + line
+        } else if (lineCount===2) {line = parseRow(line,['\t<td>','</td>\n'])
+          line = '<tr>\n' + line + '</tr>\n'
+          newBody = newBody + line
+        } else if (lineCount===3){
+          newFooter = newFooter + line
+        }
+        i = j + 1;
+      }
+
+      code = '<pre><code><table>' +
+        newHeader + newBody + '</table>' + '<h5>' + newFooter + '</h5>' +
+        '</code></pre>'
       node.type = "html"
       node.children = undefined
       node.value = code
