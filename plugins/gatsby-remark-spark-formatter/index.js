1c48985919562fd54726aa9f7efcc460f5c708b1
Jolfr <thomas.jack.carroll@gmail.com>
2020-12-22 14:04:09 -0500

    set up pasing for count formatter

--- plugins/gatsby-remark-spark-formatter/index.js
+++ plugins/gatsby-remark-spark-formatter/index.js
@@ -9,7 +9,31 @@ module.exports = ({ markdownAST }, options) => {
     const syntax = /^\d+\.*\d*$/ig
     const number = code.match(syntax);
     if (number !== null && formatNumber) {
-      code = '<pre><code><h1>'+ code + '</h1></code></pre>'
+      let splitNum = code.split(/(\d+)(\.*)(\d*)/)
+      let integer = splitNum[1]
+      let decimal = splitNum[3]
+      let digits = integer.split(/(\d)/)
+      var numString = ''
+      if (decimal !== '') {
+        numString = '.' + decimal
+      }
+      if (digits.length > 3) {
+        var end = digits.length
+        var start = 0
+        while (end > start) {
+          if (digits[end] !== undefined) {
+            numString = digits[end] + numString
+            if ((numString.length + 1) % 4 === 0 && end !== 1) {
+              numString = ',' + numString
+            }
+          }
+          end--
+        }
+      } else {
+        numString = integer + numString
+      }
+
+      code = '<pre><code><h3>'+ numString + '</h3></code></pre>'
       node.type = "html"
       node.children = undefined
       node.value = code
